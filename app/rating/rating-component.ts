cbedccf0284037873112bbe223fb41dc3ab1fe85
Gupta <madhuri.gupta@capgemini.com>
2017-12-28 11:36:40 +0530

    Rating Nested component is working

--- app/rating/rating-component.ts
+++ app/rating/rating-component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input ,OnChanges } from '@angular/core';
+
 
 
 
 @Component({
     selector: 'rating-star',
-    templateUrl: './app/rating/rating-component.html'
+    templateUrl: './app/rating/rating-component.html',
+    styleUrls: ['./app/rating/rating.component.css']
 })
 
-export class RatingComponent  {   
-    constructor() {
-
-     }
-
+export class RatingComponent implements OnChanges {  
+    starWidth: number;
+    @Input() rating : number; 
+    ngOnChanges(): void {
+        this.starWidth = this.rating * 86 / 5;
+    }
+	
+  
+    
    
 }
